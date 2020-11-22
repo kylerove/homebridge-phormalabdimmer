19d5703386d50bd335dbe4815df9835b019007bb
Kyle Rove <kylerove@lecosmos.local>
2020-11-22 20:34:43 +0000

    ugh

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ var MCP4728 = require('ncd-red-mcp4728');
 //import {AddPhormalabLamp} from "phormalab-lamp.js";
 const AddPhormalabLamp = require('./phormalab-lamp.js')
 const PLATFORM_NAME = "PhormalabDimmer";
+const PLUGIN_NAME = "homebridge-phormalabdimmer";
 let hap;
 
 // register the platform plugin
-export = (api) => {
-    hap = api.hap;
-    api.registerPlatform(PLATFORM_NAME, PhormalabDimmerPlatform);
-};
+//module.export = (api) => {
+//    hap = api.hap;
+//    api.registerPlatform(PLATFORM_NAME, PhormalabDimmerPlatform);
+//};
+module.exports = function(homebridge) {
+    console.log("homebridge API version: " + homebridge.version);
+
+    // Accessory must be created from PlatformAccessory Constructor
+    Accessory = homebridge.platformAccessory;
+
+    // Service and Characteristic are from hap-nodejs
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    // For platform plugin to be considered as dynamic platform plugin,
+    // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
+    homebridge.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, PhormalabDimmerPlatform, false);
+}
 
 // implement the platform plugin class
 class PhormalabDimmerPlatform implements StaticPlatformPlugin {
