4968badc1613dc4f281a0d8872964a780ff4c81c
Kyle Rove <kylerove@lecosmos.local>
2020-11-22 07:12:24 +0000

    Update code to mcp4728

--- index.js
+++ index.js
@@ -1,65 +1,118 @@
-var Service, Characteristic;
-var i2c = require('@abandonware/i2c');
+var comms = require('ncd-red-comm');
+var MCP4728 = require('ncd-red-mcp4728');
+var myAccessories = [];
 
 module.exports = function(homebridge) {
+    Accessory = homebridge.platformAccessory;
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory('homebridge-phormalabdimmer', 'phormalabdimmer', PhormalabDimmer);
+    UUIDGen = homebridge.hap.uuid;
+    homebridge.registerPlatform('PhormalabDimmer', PhormalabDimmer);
 };
 
-function PhormalabDimmer(log, config) {
+class PhormalabDimmer {
+    
+    contructor(log, config, api) {
+        this.log = log;
+        this.log.debug('PhormalabDimmer plugin loaded');
+        this.config = config;
+        this.api = api;
+        this.name = config.name;
+        this.i2c_address = parseInt(this.config.i2c_address, 16) || 0x60;
+        this.i2c_device = this.config.i2c_device || '/dev/i2c-1';
+        this.lampNames = this.config.lamp_names || ['Phormalab'];
+        
+        // Enable config based DEBUG logging enable
+        this.debug = config.debug || false;
+        if (this.debug) {
+            debug.enabled = true;
+        }
 
-    this.log = log;
+        this.api.on('didFinishLaunching', () => {
+            this.log("didFinishLaunching");
 
-    this.name = config.name;
-    this.i2c_address = parseInt(config.i2c_address);
-    this.i2c_device = config.i2c_device || '/dev/i2c-1';
+            this.log(`Expecting MCP4728 I²C DAC at address 0x${this.i2c_address.toString(16)} on bus ${this.i2c_device}`);
+            this.comm = new comms.NcdI2C(1);
+            this.dac = new MCP4728(this.i2c_address, this.comm, {
+                eeprom_persist_1: true,
+                eeprom_persist_2: true,
+                eeprom_persist_3: true,
+                eeprom_persist_4: true,
+            });
+        
+            for (i = 1; i < (this.lampNames.length + 1); i++) {
+                if (typeof this.lampNames[i] !== 'undefined') {
+                    debug("Creating accessory for", this.lampNames[i]);
+                    var newLamp = new PhormalabAccessory(this, this.dac, i, this.lampNames[i]);
+                    updateStatus(newLamp, i);
+                }
+            }
+        });
 
-    this.cache = {
-        'brightness': 100,
-        'state': false
-    };
+    function PhormalabAccessory(that, dac, channel, name) {
+        this.log = that.log;
+        this.name = name;
+        this.log("Determining if Phormalab lamp "+this.name+" on channel "+channel+" exists in HB database");
+        this.lampID = channel;
+        this.refresh = that.refresh;
+        this.cache = { 'brightness': 100,
+                       'state': false };
+        
+        var uuid = UUIDGen.generate(this.name + " - Phormalab");
+        
+        if (!getAccessoryByLampID(this.lampID)) {
+            this.log("Adding Phormalab lamp", this.name);
+            this.accessory = new Accessory(this.name, uuid, 10);
+            this.accessory.log = that.log;
+            this.accessory.context.lampID = channel;
+        
+            this.accessory.getService(Service.AccessoryInformation)
+                .setCharacteristic(Characteristic.Manufacturer, "Phormalab")
+                .setCharacteristic(Characteristic.Model, "Hotdoor")
+                .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name + "-" + this.lampID)
+                .setCharacteristic(Characteristic.FirmwareRevision, "MCP4728");
+            
+            this.accessory.addService(Service.Lightbulb, this.name);
+            
+            this.accessory
+                .getService(Service.Lightbulb)
+                .getCharacteristic(Characteristic.On)
+                .on('get', this.getPowerState.bind(this.accessory))
+                .on('set', this.setPowerState.bind(this.accessory));
+            
+            // this.addOptionalCharacteristic(Characteristic.HeatingThresholdTemperature);
+            this.accessory
+                .getService(Service.Lightbulb)
+                .addCharacteristic(new Characteristic.Brightness())
+                .on('get', this.getBrightness.bind(this.accessory))
+                .on('set', this.setBrightness.bind(this.accessory));
+                
+            this.accessory
+                .getService(Service.Lightbulb).log = this.log;
+                
+            //this.accessory.context.ChangeLamp = new ChangeLamp(this.accessory);
+            that.api.registerPlatformAccessories("homebridge-phormalabdimmer", "PhormalabDimmer", [this.accessory]);
+            myAccessories.push(this.accessory);
+            return this.accessory;
+        } else {
+            this.log("Existing Phormalab accessory", this.name);
+            return getAccessoryByLampID(this.lampID);
+        }
+    }
+    
+    function getAccessoryByLampID(lampID) {
+        var value;
+        myAccessories.forEach(function(accessory) {
+            // debug("getAccessoryByName zone", accessory.name, name);
+            if (accessory.context.lampID === lampID) {
+                value = accessory;
+            }
+        });
+        
+        return value;
+    }
 
-    this.wire = new i2c(this.i2c_address, {
-        device: this.i2c_device
-    });
-
-    this.services = {};
-}
-
-PhormalabDimmer.prototype = {
-
-    /** Required Functions **/
-    identify: function(callback) {
-        this.log('Identify requested!');
-        callback();
-    },
-
-    getServices: function() {
-        this.services.informationService = new Service.AccessoryInformation();
-
-        this.services.informationService
-            .setCharacteristic(Characteristic.Manufacturer, 'Phormalab')
-            .setCharacteristic(Characteristic.Model, 'Hotdoor')
-            .setCharacteristic(Characteristic.SerialNumber, this.name);
-
-        this.log('creating Phormalab');
-        this.services.lightbulbService = new Service.Lightbulb(this.name);
-
-        this.services.lightbulbService
-            .getCharacteristic(Characteristic.On)
-            .on('get', this.getPowerState.bind(this))
-            .on('set', this.setPowerState.bind(this));
-
-        this.services.lightbulbService
-            .addCharacteristic(new Characteristic.Brightness())
-            .on('get', this.getBrightness.bind(this))
-            .on('set', this.setBrightness.bind(this));
-
-        return [this.services.informationService, this.services.lightbulbService];
-    },
-
-    getPowerState: function(callback) {
+    function getPowerState(callback) {
         this.getBrightness(function(err, brightness) {
             if (err) {
                 this.log('Error (getPowerState): '+err);
@@ -78,7 +131,7 @@ PhormalabDimmer.prototype = {
             }
         }.bind(this));
     },
-    setPowerState: function(state, callback) {
+    function setPowerState(state, callback) {
         if (state && !this.cache.state) {
             this.setBrightness(100, function(err) {
                 if (err) {
@@ -105,63 +158,22 @@ PhormalabDimmer.prototype = {
             this.log('Error (setPowerState): unexpected no action taken');
             callback();
         }
-    },
-    readInteger: function(address, callback) {
-        this.wire.readBytes(address, 2, function(error, res) {
-            if (error) {
-                this.log('Error (readInteger): '+error);
-                callback(error);
-            } else {
-                let value = res[0];
-                this.log('readInteger: '+parseInt((value << 8) || res[1]));
-                callback(null, parseInt((value << 8) || res[1]));
-            }
-        }.bind(this));
-    },
-    writeInteger: function(address, value, callback) {
-        bytes = [];
-        bytes[0] = (value >> 8) & 0xFF;
-        bytes[1] = value & 0xFF;
-
-        this.wire.writeBytes(address, bytes, function(error) {
-            if (error) {
-                this.log('Error (writeInteger): '+error);
-                callback(error);
-                return;
-            }
-            
-            this.log('writeInteger: '+bytes[0]+' '+bytes[1]);
-            callback();
-        });
-    },
-    readBrightness: function(callback) {
-        this.readInteger(this.brightness, function(err, brightness) {
-            if (err) {
-                this.log('Error (readBrightness): '+err);
-                callback(err);
-                return;
-            }
-            this.log('readBrightness: '+this.brightness);
-        }.bind(this));
-    },
-    setBrightness: function(brightness, callback) {
-        this.cache.brightness = brightness;
-        this.writeInteger(this.brightness, brightness, function(err) {
-            if (err) {
-                this.log('Error (setBrightness): '+err);
-                callback(err);
-            } else {
-                this.log('setBrightness: '+brightness);
-                this.getPowerState(function(err, val){
-                    if (!err) {
-                        this.services.lightbulbService
-                            .getCharacteristic(Characteristic.On)
-                            .updateValue(val);
-                    }
-                }.bind(this));
-                callback();
-            }
-        }.bind(this));
     }
-
-};
+    
+    function readBrightness(callback) {
+        dac.get().then((r) => {
+            console.log(r);
+            console.log('Get brightness: ' + brightness);
+            setTimeout(setBrightness, 5000);
+        }).catch(console.log);
+    }
+    
+    function setBrightness(brightness, callback) {
+        this.cache.brightness = brightness;
+        dac.set(brightness, this.lampID, true).then((r) => {
+            console.log(r);
+            console.log('Set brightness: ' + brightness);
+            setTimeout(readBrightness, 500);
+        }).catch(console.log);
+    }
+}
