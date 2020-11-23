4e4bb096099ef799d963bbbac635643101d39c92
Kyle Rove <kylerove@lecosmos.local>
2020-11-23 06:04:02 +0000

    error handling

--- index.js
+++ index.js
@@ -30,22 +30,15 @@ class PhormalabDimmerPlatform  {
             log.error("Phormalab Dimmer configuration only supports up to four `lamp_names` connected to the four MCP4728 DAC outputs.")
         }
 
+        // initialize communications stack and dac object to communicate with mcp4728
         log.info(`Expecting MCP4728 I²C DAC at address 0x${this.i2c_address.toString(16)} on bus ${this.i2c_device}`);
         this.comm = new comms.NcdI2C(1);
-        //log.info(this.comm);
-        try {
-            this.dac = new MCP4728(this.i2c_address, this.comm, {
-                eeprom_persist_1: true,
-                eeprom_persist_2: true,
-                eeprom_persist_3: true,
-                eeprom_persist_4: true,
-            });
-        }
-        catch(err) {
-            log.error(err);
-            //log.error('MCP4728 was not reachable on the I²C bus. Check to make sure it is connected.');
-        }
-        //log.info(this.dac);
+        this.dac = new MCP4728(this.i2c_address, this.comm, {
+            eeprom_persist_1: true,
+            eeprom_persist_2: true,
+            eeprom_persist_3: true,
+            eeprom_persist_4: true,
+        });
         
         // check if dac initialized
         if (!this.dac.initialized) {
